26bb46f8d9ff21f6601864da61618f6a91863be6
keshiindungi <joan03267@gmail.com>
2025-10-22 00:04:06 +0300

    Added run section for Render dynamic port

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routers.recommend import router as recommend_router  
+from backend.routers.recommend import router as recommend_router
+import os
+import uvicorn
 
 app = FastAPI(
     title="NutriAI - AI-Powered Nutrition Recommendation System",
@@ -12,22 +13,26 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000", 
+        "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://localhost:5173", 
+        "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:5174",
-        "http://127.0.0.1:5174"
-    ],  
+        "http://127.0.0.1:5174",
+        "https://nutri-ai-frontend.onrender.com"  # add your frontend Render URL later
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 app.include_router(recommend_router, tags=["Recommendation"])
-
 
 @app.get("/")
 def home():
     return {"message": "Welcome to NutriAI API!"}
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
